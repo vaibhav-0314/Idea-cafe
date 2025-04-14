47189c08eaefbc2c0a82f7c2b9f63d3edbfdcf6d
sumangalagouda <sumangalagouda@gmail.com>
2025-04-14 19:20:46 +0530

    Added json files

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,3 @@
-/*import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  base: '/Ideal-Cafe/', // Add your repository name here
-});*/
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -22,5 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/Ideal-Cafe/', 
+  base: '/Ideal-Cafe/', // GitHub repo name
 });
